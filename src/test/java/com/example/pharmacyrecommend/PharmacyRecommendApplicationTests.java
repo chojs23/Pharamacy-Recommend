2a435e90bc1c6aa9e3c3945f3a9de9e38a628ec2
neo <chojs990222@gmail.com>
2022-11-21 22:27:29 +0900

    initial commit

---
+++ src/test/java/com/example/pharmacyrecommend/PharmacyRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
