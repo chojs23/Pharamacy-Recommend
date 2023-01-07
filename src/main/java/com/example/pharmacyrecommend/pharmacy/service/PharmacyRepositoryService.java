bce0043b1e68cbefa092a206ae9504a3a4a75714
chojs23 <chojs990222@gmail.com>
2023-01-07 16:14:58 +0900

    Enable JPA Auditing, init data

--- src/main/java/com/example/pharmacyrecommend/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/pharmacyrecommend/pharmacy/service/PharmacyRepositoryService.java
@@ -2,6 +2,7 @@ package com.example.pharmacyrecommend.pharmacy.service;
 
 import com.example.pharmacyrecommend.pharmacy.entity.Pharmacy;
 import com.example.pharmacyrecommend.pharmacy.repository.PharmacyRepository;
+import java.util.List;
 import java.util.Objects;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +27,18 @@ public class PharmacyRepositoryService {
         entity.changePharmacyAddress(address);
     }
 
-    public void updatePharmacyAddressWithoutTransaction(Long id, String address){
+    public void updatePharmacyAddressWithoutTransaction(Long id, String address) {
         Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
 
-        if(Objects.isNull(entity)){
+        if (Objects.isNull(entity)) {
             log.error("Pharmacy is not found id: {}", id);
             return;
         }
-
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
--- src/test/groovy/com/example/pharmacyrecommend/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacyrecommend/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -55,12 +55,34 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         def result = pharmacyRepository.saveAll([pharmacy])
 
         then:
-        result.size()==1
+        result.size() == 1
 
         result.get(0).getPharmacyAddress() == address
         result.get(0).getPharmacyName() == name
         result.get(0).getLatitude() == latitude
         result.get(0).getLongitude() == longitude
+    }
+
+    def "BaseTimeEntity save"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 127.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getCreatedDate() != null
+        result.getModifiedDate() != null
     }
 
 
