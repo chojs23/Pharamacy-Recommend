1c1ab3e83e989870c60c2740bf52c1e4b78e7f90
chojs23 <chojs990222@gmail.com>
2023-01-08 19:42:15 +0900

    kakao api Spring retry test code

--- src/main/java/com/example/pharmacyrecommend/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/example/pharmacyrecommend/api/service/KakaoAddressSearchService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -18,27 +20,39 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class KakaoAddressSearchService {
 
-    private final RestTemplate restTemplate;
-    private final KakaoUriBuilderService kakaoUriBuilderService;
+  private final RestTemplate restTemplate;
+  private final KakaoUriBuilderService kakaoUriBuilderService;
 
-    @Value("${kakao.rest.api.key}")
-    private String kakaoRestApiKey;
+  @Value("${kakao.rest.api.key}")
+  private String kakaoRestApiKey;
 
-    public KakaoApiResponseDto requestAddressSearch(String address) {
-        if(ObjectUtils.isEmpty(address)) return null;
-        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
-
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.add(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
-        HttpEntity httpEntity = new HttpEntity<>(headers);
-
-        // kakao api 호출
-
-        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
-
-
+  @Retryable(
+      value = {RuntimeException.class},
+      maxAttempts = 2,
+      backoff = @org.springframework.retry.annotation.Backoff(delay = 2000)
+  )
+  public KakaoApiResponseDto requestAddressSearch(String address) {
+    if (ObjectUtils.isEmpty(address)) {
+      return null;
     }
+    URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.add(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+    HttpEntity httpEntity = new HttpEntity<>(headers);
+
+    // kakao api 호출
+
+    return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class)
+        .getBody();
+  }
+
+  @Recover
+  public KakaoApiResponseDto recover(RuntimeException e, String address) {
+    log.error("All the retries failed, address: {}, error: {}", address, e.getMessage());
+    return null;
+  }
 
 
 }
